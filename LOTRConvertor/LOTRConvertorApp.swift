462dca4bb21c97f52ac41ba45bea75a8d0076025
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-03-11 10:36:41 +0500

    Initial Commit

---
+++ LOTRConvertor/LOTRConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConvertorApp.swift
+//  LOTRConvertor
+//
+//  Created by G'aniyev Muhammad on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
