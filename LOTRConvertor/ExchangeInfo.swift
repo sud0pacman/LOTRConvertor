1fe8cb8f8cd283fcd822ec230d1a8331a4852d88
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-03-14 13:18:16 +0500

    - Creat new SwiftUI view - Review stacks - Review adding & modifying views - Test in dark mode

---
+++ LOTRConvertor/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConvertor
+//
+//  Created by G'aniyev Muhammad on 14/03/26.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
