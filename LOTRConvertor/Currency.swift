bac7c7209da7f583d5dc521bb03926e6e4b6b3f9
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-03-27 11:31:05 +0500

    - Computed property - Framework - Conditionals - self

---
+++ LOTRConvertor/Currency.swift
@@ -0,0 +1,46 @@
+//
+//  Currency.swift
+//  LOTRConvertor
+//
+//  Created by G'aniyev Muhammad on 25/03/26.
+//
+
+import SwiftUI
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+    
+    var image: ImageResource {
+        switch self {
+        case .copperPenny:
+            return .copperpenny
+        case .silverPenny:
+            return .silverpenny
+        case .silverPiece:
+            return .silverpiece
+        case .goldPenny:
+            return .goldpenny
+        case .goldPiece:
+            return .goldpiece
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
+}
